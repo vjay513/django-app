de1c880b99e2f55bd5316c9dcc76cc6a8b273afe
Vijay Bammidi <vijay.bammidi@thomsonreuters.com>
2020-06-18 23:44:05 +0530

    model

--- polls/admin.py
+++ polls/admin.py
@@ -2,7 +2,20 @@ from django.contrib import admin
 from polls.models import AccessRecord, Topic, Webpage, Person
 
 # Register your models here.
-admin.site.register(AccessRecord)
-admin.site.register(Person)
-admin.site.register(Topic)
-admin.site.register(Webpage)
+
+class PersonAdmin(admin.ModelAdmin):
+    fields = ['first_name', 'last_name']
+
+class TopicAdmin(admin.ModelAdmin):
+    fields = ['top_name']
+
+class WebpageAdmin(admin.ModelAdmin):
+    fields = ["topic", "name", "url"]
+
+class AccessRecordAdmin(admin.ModelAdmin):
+    fields = ["name", "date"]
+
+admin.site.register(AccessRecord, AccessRecordAdmin)
+admin.site.register(Person, PersonAdmin)
+admin.site.register(Topic, TopicAdmin)
+admin.site.register(Webpage, WebpageAdmin)
